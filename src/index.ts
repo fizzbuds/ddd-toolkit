64d449b15628313961a9141a8f6323d4338bd72c
Gabriele Toselli <toselli.gabriele@gmail.com>
2023-11-16 08:25:10 +0100

    refactor: extract files from infrastructure folder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-export * from './infrastructure/serializer.interface';
-export * from './infrastructure/mongo-aggregate-repo';
-export * from './infrastructure/repo-hooks';
-export * from './infrastructure/generic-id';
-export { OptimisticLockError } from './infrastructure/errors';
-export { DuplicatedIdError } from './infrastructure/errors';
-export * from './infrastructure/mongo-query-repo';
+export * from './serializer.interface';
+export * from './mongo-aggregate-repo';
+export * from './repo-hooks';
+export * from './generic-id';
+export * from './errors';
+export * from './mongo-query-repo';
